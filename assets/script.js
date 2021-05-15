3020dcc0e9733bb4bf9d7b32b87463fff6a1d1d2
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-05-14 21:52:15 -0400

    added basic styling and js variables

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+let quiz = $('#quiz');
+let scores = $('#scores');
+let quizNav = $('#quizNav');
+let scoresNav = $('#scoresNav');
+
+quizNav.addEventListener()
+
+if($(window).width() < 767)
+{
+   // change functionality for smaller screens
+
+} else {
+   // change functionality for larger screens
+}
+
+function showQuiz() {
+
+}
+
+function showScores() {
+
+}
